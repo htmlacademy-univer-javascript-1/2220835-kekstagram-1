aebc69aa6c49372de90cc210091e42de428a5c98
DariInel <adara21210@gmail.com>
2022-09-21 19:04:21 +0500

    2 functions created

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 function getRandomNumber(start, end){
-	if(start == end) return start;
-	if(start > end) return -1;
-	start = Math.ceil(start);
-	end = Math.floor(end);
-	return Math.floor(Math.random() * (end - start)) + start;
+    if(start === end) {
+        return start;
+    }
+    if(start > end) {
+        return -1;
+    }
+    start = Math.ceil(start);
+    end = Math.floor(end);
+    return Math.floor(Math.random() * (end - start)) + start;
 }
 
 function isLineMatch(str, maxLength){
-	if (str.length <= maxLength) return true;
-	return false;
+    if (str.length <= maxLength) {
+        return true;
+    }
+    return false;
 }
