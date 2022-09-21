fb6f27d38225a3afad3cba38bc1a8ff64bbda074
DariInel <adara21210@gmail.com>
2022-09-21 18:51:10 +0500

    js file included and 2 functions created

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function getRandomNumber(start, end){
+	if(start == end) return start;
+	if(start > end) return -1;
+	start = Math.ceil(start);
+	end = Math.floor(end);
+	return Math.floor(Math.random() * (end - start)) + start;
+}
+
+function isLineMatch(str, maxLength){
+	if (str.length <= maxLength) return true;
+	return false;
+}
