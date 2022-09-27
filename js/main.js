a767105e47be9523f59dff82fa7115ae0c466bdb
DariInel <adara21210@gmail.com>
2022-09-27 22:21:07 +0500

    Подключен js файл и добавлены 2 вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function getRandomNumber(start, end)
+{
+  if(start === end){return start;}
+  if(start > end){return -1;}
+  start = Math.ceil(start);
+  end = Math.floor(end);
+  return Math.floor(Math.random() * (end - start)) + start;
+}
+
+function checkMaxLength(str, maxLength)
+{
+  if(str.length <= maxLength){return true;}
+  return false;
+}
+
+getRandomNumber(1, 10);
+checkMaxLength(' ', 5);
